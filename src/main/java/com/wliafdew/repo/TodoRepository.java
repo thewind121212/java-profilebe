39047af5d584ef93643f196a0703cdadae2e3052
locgacui <locgacui@gmail.com>
2025-06-14 10:34:37 +0700

    add model users and service userService

--- src/main/java/com/wliafdew/repo/TodoRepository.java
+++ src/main/java/com/wliafdew/repo/TodoRepository.java
@@ -1,107 +1,107 @@
-package com.wliafdew.repo;
+// package com.wliafdew.repo;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+// import java.sql.Connection;
+// import java.sql.PreparedStatement;
+// import java.sql.ResultSet;
+// import java.sql.SQLException;
+// import java.sql.Statement;
+// import java.util.ArrayList;
+// import java.util.List;
 
-import org.springframework.stereotype.Repository;
+// import org.springframework.stereotype.Repository;
 
-import com.wliafdew.model.Todo;
+// import com.wliafdew.model.Todo;
 
-@Repository
-public class TodoRepository {
-    private final Database database;
+// @Repository
+// public class TodoRepository {
+//     private final Database database;
 
-    public TodoRepository(Database database) {
-        this.database = database;
-    }
+//     public TodoRepository(Database database) {
+//         this.database = database;
+//     }
 
-    public List<Todo> findAll() {
-        List<Todo> todos = new ArrayList<>();
-        try (Connection conn = database.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT * FROM todos")) {
+//     public List<Todo> findAll() {
+//         List<Todo> todos = new ArrayList<>();
+//         try (Connection conn = database.getConnection();
+//              Statement stmt = conn.createStatement();
+//              ResultSet rs = stmt.executeQuery("SELECT * FROM todos")) {
             
-            while (rs.next()) {
-                todos.add(new Todo(
-                    rs.getLong("id"),
-                    rs.getString("title"),
-                    rs.getString("description"),
-                    rs.getBoolean("isDone")
-                ));
-            }
-        } catch (SQLException e) {
-            System.out.println("Error fetching todos: " + e.getMessage());
-        }
-        return todos;
-    }
+//             while (rs.next()) {
+//                 todos.add(new Todo(
+//                     rs.getLong("id"),
+//                     rs.getString("title"),
+//                     rs.getString("description"),
+//                     rs.getBoolean("isDone")
+//                 ));
+//             }
+//         } catch (SQLException e) {
+//             System.out.println("Error fetching todos: " + e.getMessage());
+//         }
+//         return todos;
+//     }
 
-    public Todo findById(Long id) {
-        try (Connection conn = database.getConnection();
-             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM todos WHERE id = ?")) {
+//     public Todo findById(Long id) {
+//         try (Connection conn = database.getConnection();
+//              PreparedStatement stmt = conn.prepareStatement("SELECT * FROM todos WHERE id = ?")) {
             
-            stmt.setLong(1, id);
-            try (ResultSet rs = stmt.executeQuery()) {
-                if (rs.next()) {
-                    return new Todo(
-                        rs.getLong("id"),
-                        rs.getString("title"),
-                        rs.getString("description"),
-                        rs.getBoolean("isDone")
-                    );
-                }
-            }
-        } catch (SQLException e) {
-            System.out.println("Error fetching todo: " + e.getMessage());
-        }
-        return null;
-    }
+//             stmt.setLong(1, id);
+//             try (ResultSet rs = stmt.executeQuery()) {
+//                 if (rs.next()) {
+//                     return new Todo(
+//                         rs.getLong("id"),
+//                         rs.getString("title"),
+//                         rs.getString("description"),
+//                         rs.getBoolean("isDone")
+//                     );
+//                 }
+//             }
+//         } catch (SQLException e) {
+//             System.out.println("Error fetching todo: " + e.getMessage());
+//         }
+//         return null;
+//     }
 
-    public Todo save(Todo todo) {
-        String sql = todo.getId() == null ?
-            "INSERT INTO todos (title, description, isDone) VALUES (?, ?, ?)" :
-            "UPDATE todos SET title = ?, description = ?, isDone = ? WHERE id = ?";
+//     public Todo save(Todo todo) {
+//         String sql = todo.getId() == null ?
+//             "INSERT INTO todos (title, description, isDone) VALUES (?, ?, ?)" :
+//             "UPDATE todos SET title = ?, description = ?, isDone = ? WHERE id = ?";
 
-        try (Connection conn = database.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+//         try (Connection conn = database.getConnection();
+//              PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             
-            stmt.setString(1, todo.getTitle());
-            stmt.setString(2, todo.getDescription());
-            stmt.setBoolean(3, todo.getIsDone());
+//             stmt.setString(1, todo.getTitle());
+//             stmt.setString(2, todo.getDescription());
+//             stmt.setBoolean(3, todo.getIsDone());
             
-            if (todo.getId() != null) {
-                stmt.setLong(4, todo.getId());
-            }
+//             if (todo.getId() != null) {
+//                 stmt.setLong(4, todo.getId());
+//             }
 
-            stmt.executeUpdate();
+//             stmt.executeUpdate();
 
-            if (todo.getId() == null) {
-                try (ResultSet rs = stmt.getGeneratedKeys()) {
-                    if (rs.next()) {
-                        todo.setId(rs.getLong(1));
-                    }
-                }
-            }
-            return todo;
-        } catch (SQLException e) {
-            System.out.println("Error saving todo: " + e.getMessage());
-            return null;
-        }
-    }
+//             if (todo.getId() == null) {
+//                 try (ResultSet rs = stmt.getGeneratedKeys()) {
+//                     if (rs.next()) {
+//                         todo.setId(rs.getLong(1));
+//                     }
+//                 }
+//             }
+//             return todo;
+//         } catch (SQLException e) {
+//             System.out.println("Error saving todo: " + e.getMessage());
+//             return null;
+//         }
+//     }
 
-    public boolean deleteById(Long id) {
-        try (Connection conn = database.getConnection();
-             PreparedStatement stmt = conn.prepareStatement("DELETE FROM todos WHERE id = ?")) {
+//     public boolean deleteById(Long id) {
+//         try (Connection conn = database.getConnection();
+//              PreparedStatement stmt = conn.prepareStatement("DELETE FROM todos WHERE id = ?")) {
             
-            stmt.setLong(1, id);
-            return stmt.executeUpdate() > 0;
-        } catch (SQLException e) {
-            System.out.println("Error deleting todo: " + e.getMessage());
-            return false;
-        }
-    }
-} 
+//             stmt.setLong(1, id);
+//             return stmt.executeUpdate() > 0;
+//         } catch (SQLException e) {
+//             System.out.println("Error deleting todo: " + e.getMessage());
+//             return false;
+//         }
+//     }
+// } 
---
+++ src/main/java/com/wliafdew/service/usersService.java
@@ -0,0 +1,21 @@
+package com.wliafdew.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.wliafdew.exception.ResourceNotFoundException;
+import com.wliafdew.model.users;
+
+public interface usersService {
+    List<users> getUsers();
+
+    users getUserByID(UUID id) throws ResourceNotFoundException;
+
+    users insertUser(users user);
+
+    users updateUsers(users user);
+
+    void deleteUser(UUID id) throws ResourceNotFoundException;
+
+    users getUserByEmail(String email) throws ResourceNotFoundException;
+}
